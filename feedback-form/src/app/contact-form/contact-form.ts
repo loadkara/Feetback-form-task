b42dba7ba689a7cba05d6fb4099e52191413ae87
KaraLoad <loadkara@gmail.com>
2025-09-12 01:52:16 +0300

    feat: add client validation

--- feedback-form/src/app/contact-form/contact-form.ts
+++ feedback-form/src/app/contact-form/contact-form.ts
@@ -1,11 +1,29 @@
 import { Component } from '@angular/core';
+import { ReactiveFormsModule, FormBuilder, Validators, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-contact-form',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './contact-form.html',
   styleUrl: './contact-form.css'
 })
 export class ContactForm {
+  feedbackForm: FormGroup;
 
+  constructor(private fb: FormBuilder) {
+    this.feedbackForm = this.fb.group({
+      name: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      phone: ['', [Validators.required, Validators.pattern(/^\+?\d+$/)]],
+      topic: ['', Validators.required],
+      message: ['', Validators.required],
+      captcha: ['', Validators.required]
+    });
+  }
+
+  onSubmit() {
+    if (!this.feedbackForm.valid) {
+      alert('Заполните все поля корректно');
+    }
+  }
 }
