3dfb317c3e4ded9290daf8b93d44a8c719285b7d
KaraLoad <loadkara@gmail.com>
2025-09-15 11:40:37 +0300

    fix:add PORT support

--- backend/Feedback.Api/Program.cs
+++ backend/Feedback.Api/Program.cs
@@ -3,6 +3,9 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
+var port = Environment.GetEnvironmentVariable("PORT") ?? "5000";
+builder.WebHost.UseUrls($"http://0.0.0.0:{port}");
+
 builder.Services.AddControllers();
 
 builder.Services.AddCors(options =>
