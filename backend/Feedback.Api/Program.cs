17b0ea3239dc581526d3a22675a94e4b1507472c
KaraLoad <loadkara@gmail.com>
2025-09-15 04:09:45 +0300

    fix: program

--- backend/Feedback.Api/Program.cs
+++ backend/Feedback.Api/Program.cs
@@ -3,10 +3,8 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Добавляем контроллеры
 builder.Services.AddControllers();
 
-// Настройка CORS
 builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowAngular", policy =>
@@ -17,18 +15,15 @@ builder.Services.AddCors(options =>
     });
 });
 
-// Подключаем ApplicationDbContext
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
-// Активация CORS
-app.UseCors("AllowAngular");
 
-// Обязательные middleware
+
 app.UseHttpsRedirection();
+app.UseCors("AllowAngular");
 app.UseAuthorization();
-app.MapControllers(); // Важно: чтобы работали контроллеры
-
+app.MapControllers();
 app.Run();
